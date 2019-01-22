ef73c0b4bc2f6950e953a69938fea7e8b69d446a
Carl Smith <carlfsmithiii@gmail.com>
2019-01-21 15:19:51 -1000

    run -- add 2 albums and list them

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        Playlist playlist = new Playlist();
+        playlist.addAlbum();
+        playlist.addAlbum();
+        playlist.displayAlbums(); 
     }
 }
